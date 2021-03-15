c023c4d6a9cefdd5e68e642aaca475a28a291011
effervescinn <32834764+effervescinn@users.noreply.github.com>
2021-03-15 20:11:15 +0300

    проверка хуков

--- main.c
+++ main.c
@@ -24,25 +24,48 @@
 // } 
 
 #include <mlx.h>
+#include <stdio.h>
 
 typedef struct  s_vars {
         void    *mlx;
         void    *win;
+        int i;
 }               t_vars;
 
 
-int     render_next_frame(void *YourStruct)
+// int     render_next_frame(void *YourStruct)
+// {
+//     YourStruct->win = mlx_new_window(vars.mlx, 1920, 1080, "Hello world!");
+// }
+
+int     key_hook(int keycode, t_vars *vars)
 {
-    YourStruct->win = mlx_new_window(vars.mlx, 1920, 1080, "Hello world!");
+    // int i = 0;
+
+    if (keycode == 123)
+    {
+        mlx_pixel_put(vars->mlx, vars->win, vars->i, 10, 0xFFFFFF);
+    }
+    (vars->i)++;
+    // else if (keycode == 124)
+    // {
+    //     mlx_pixel_put(vars->mlx, vars->win, i + 1, 10, 0xFFFFFF);
+    // }
+    return (0);
 }
+
 int     main(void)
 {
     t_vars      vars;
     // void    *mlx;
     // void    *mlx_win;
+    vars.i = 1;
 
     vars.mlx = mlx_init();
     
-    mlx_loop_hook(vars.mlx, render_next_frame, &vars);
+    // mlx_loop_hook(vars.mlx, render_next_frame, &vars);
+    vars.mlx = mlx_init();
+    vars.win = mlx_new_window(vars.mlx, 640, 480, "Hello world!");
+    mlx_key_hook(vars.win, key_hook, &vars);
     mlx_loop(vars.mlx);
 }
